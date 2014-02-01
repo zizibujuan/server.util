9e14a6a17a118f612ccae357ad5a18d01fa1590b
jzw <zhengwei.jin@gmail.com>
2014-02-01 08:22:11 +0800

    重新为活动类型编码

--- src/com/zizibujuan/drip/server/util/constant/ActionType.java
+++ src/com/zizibujuan/drip/server/util/constant/ActionType.java
@@ -2,6 +2,7 @@ package com.zizibujuan.drip.server.util.constant;
 
 /**
  * 活动类型
+ * 
  * @author jinzw
  * @since 0.0.1
  */
@@ -10,24 +11,29 @@ public abstract class ActionType {
 	// 要存储对应的label
 	
 	/**
-	 * 新建习题
+	 * 新建习题草稿
 	 */
-	public static final String SAVE_EXERCISE = "0001";
+	public static final String SAVE_EXERCISE_DRAFT = "0001";
 	
 	/**
-	 * 回答习题
+	 * 编辑习题草稿
 	 */
-	public static final String ANSWER_EXERCISE = "0002";
-
+	public static final String EDIT_EXERCISE_DRAFT = "0002";
+	
 	/**
-	 * 编辑习题
+	 * 发布习题
 	 */
-	public static final String EDIT_EXERCISE = "0021";
+	public static final String PUBLISH_EXERCISE = "0011";
+	
+	/**
+	 * 解答习题
+	 */
+	public static final String ANSWER_EXERCISE = "0021";
 	
 	/**
 	 * 编辑答案
 	 */
-	public static final String EDIT_ANSWER = "0022";
+	public static final String EDIT_EXERCISE_ANSWER = "0022";
 	
 	
 }
